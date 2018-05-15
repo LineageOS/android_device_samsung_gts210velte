12e6d554815213aa09d253f67e8d113962811d05
LuK1337 <priv.luk@gmail.com>
2018-05-15 17:12:48 +0200

    gts210velte: Update build fingerprint
    Change-Id: Ia5e20271bb75946979c1cf663ad433eebe1da44b

--- lineage.mk
+++ lineage.mk
@@ -14,6 +14,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=gts210veltexx \
-    PRIVATE_BUILD_DESC="gts210veltexx-user 7.0 NRD90M T819XXU2BQD3 release-keys"
+    PRIVATE_BUILD_DESC="gts210veltexx-user 7.0 NRD90M T819XXU2BRB2 release-keys"
 
-BUILD_FINGERPRINT := "samsung/gts210veltexx/gts210velte:7.0/NRD90M/T819XXU2BQD3:user/release-keys"
+BUILD_FINGERPRINT := "samsung/gts210veltexx/gts210velte:7.0/NRD90M/T819XXU2BRB2:user/release-keys"
